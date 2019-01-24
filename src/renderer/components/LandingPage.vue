31757c4a9238eb85b2826a503c215dabae187229
vito <vitozhangyf@outlook.com>
2019-01-24 11:51:37 +0800

    test

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -8,7 +8,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project
+          Welcome to your new project11
         </span>
         <system-information></system-information>
       </div>
