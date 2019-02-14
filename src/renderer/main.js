e96cffb0dff3721682b3d02400aff2d119d84bc2
vito <vitozhangyf@outlook.com>
2019-02-14 11:47:14 +0800

    增加不同分辨率适配

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import axios from 'axios';
+import electron from 'electron';
 
 import App from './App';
 import router from './router';
@@ -9,6 +10,17 @@ import './assets/scss/global.scss';
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+
+// 屏幕适配
+const devInnerHeight = 1080.0; // 开发时的InnerHeight
+const devDevicePixelRatio = 1.0; // 开发时的devicepixelratio
+const devScaleFactor = 1.3; // 开发时的ScaleFactor
+const { scaleFactor } = electron.screen.getPrimaryDisplay();
+const zoomFactor =
+  (window.innerHeight / devInnerHeight) *
+  (window.devicePixelRatio / devDevicePixelRatio) *
+  (devScaleFactor / scaleFactor);
+electron.webFrame.setZoomFactor(zoomFactor);
 
 /* eslint-disable no-new */
 new Vue({
